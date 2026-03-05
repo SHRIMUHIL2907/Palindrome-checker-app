dc53c9ae2a9c647fb6a9482cac666e4eb309e330
SHRIMUHIL2907 <shrimuhil127@gmail.com>
2026-03-05 14:55:42 +0530

    its is use to print

---
+++ app/src/PalindromeCheckerApp.java
@@ -0,0 +1,7 @@
+public class PalindromeCheckerApp {
+    static void main(String[] args) {
+        System.out.println("Welcome to the Palindrom Checker Managament Syster");
+        System.out.println("Version : 1.0");
+        System.out.println("System initialized successfully");
+    }
+}
